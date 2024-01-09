349b4719711583de8109485b6522890944bfc681
JustAnEric <ericmuzyk@icloud.com>
2024-01-09 12:41:15 +1100

    new AmikaPi();

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from _logging import *
+from time import sleep
+import requests,sys
+
+version = '8.12.11'
+
+if not sys.platform.startswith('linux'):
+    log(f"Not a Linux system or supported distribution. You are on {Fore.GREEN}{sys.platform}{Fore.RESET}.", parent=parents.ERROR, time=False)
+    exit(0)
+else:
+    log(f"Running on a Linux system or supported distribution. You are on {Fore.GREEN}{sys.platform}{Fore.RESET}.", parent=parents.SIGNAL, time=False)
+
+log("Starting AmikaPi server...", parent=parents.AMIKAPI)
+try:
+    _connect_test = requests.get('https://google.com/connect', timeout=5)
+except requests.exceptions.HTTPError or requests.exceptions.Timeout: log("No internet. Unable to start the server..", parent=parents.ERROR); exit(0)
+log(f""" {Fore.GREEN}*{Fore.RESET}{Style.BRIGHT} ABOUT     {Fore.BLUE}AmikaPi/{version}{Fore.RESET} (built for Linux ARM, 64/32 bit) {Style.RESET_ALL}
+  {Fore.GREEN}*{Fore.RESET}{Style.BRIGHT} LIBS      {Fore.YELLOW}unavailable {Fore.RESET}{Style.RESET_ALL}
+  {Fore.GREEN}*{Fore.RESET}{Style.BRIGHT} CPU       {Fore.RED}statistics unavailable {Fore.RESET}{Style.RESET_ALL}
+  {Fore.GREEN}*{Fore.RESET}{Style.BRIGHT} MEMORY    {Fore.RED}statistics unavailable {Fore.RESET}{Style.RESET_ALL}""", parent=parents.NOPARENT, time=False)
---
+++ _logging.py
@@ -0,0 +1,14 @@
+from colorama import Fore, Style, Back, init
+from datetime import datetime
+from typing import Literal
+from enum import Enum
+init(autoreset=True)
+
+class parents():
+    AMIKAPI = f"{Back.CYAN}{Style.BRIGHT}{Fore.WHITE} amikapi   {Back.RESET}{Style.RESET_ALL} "
+    SIGNAL = f"{Back.YELLOW}{Style.BRIGHT}{Fore.WHITE} signal    {Back.RESET}{Style.RESET_ALL} "
+    ERROR = f"{Back.RED}{Style.BRIGHT}{Fore.WHITE} error     {Back.RESET}{Style.RESET_ALL} "
+    NOPARENT = ""
+
+def log(message:str, parent=parents.AMIKAPI,time=True):
+    print(f"{f'[{Style.DIM}{datetime.now()}{Style.RESET_ALL}] ' if time else ''}{parent}{message}")
